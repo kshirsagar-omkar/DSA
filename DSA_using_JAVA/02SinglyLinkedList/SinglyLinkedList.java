0d91743e61432d731091fb80356d367571d1f6aa
Omkar Kshirsagar <omkarkshirsagar2005@gmail.com>
2024-12-19 00:43:52 +0530

    Singly Linked List Operations

--- DSA_using_JAVA/02SinglyLinkedList/SinglyLinkedList.java
+++ DSA_using_JAVA/02SinglyLinkedList/SinglyLinkedList.java
@@ -28,7 +28,33 @@ public class SinglyLinkedList {
         return head;
     }
 
-//    ==================================================================================================================
+
+//    EXECUTION==================================================================================================================
+
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.create(3);
+        list.display();
+        System.out.println(list.getLength());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    CREATE==================================================================================================================
 
     public void create(Integer n) {
 
@@ -61,7 +87,9 @@ public class SinglyLinkedList {
     }
 
 
-//    ==================================================================================================================
+
+
+//    DISPLAY==================================================================================================================
 
 
     public void display(){
@@ -74,12 +102,107 @@ public class SinglyLinkedList {
     }
 
 
-    public static void main(String[] args) {
-        SinglyLinkedList list = new SinglyLinkedList();
 
-        list.create(3);
-        list.display();
-        System.out.println(list.getLength());
+
+//    ADD_AT_HEAD==================================================================================================================
+
+    public void addAtHead(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+        ++length;
     }
-    
+
+
+
+
+//    ADD_AT_TAIL==================================================================================================================
+
+    public void addAtTail(int data){
+        if(head == null){
+            head = new Node(data);
+        }
+        else {
+            Node curr = head;
+            while(curr.next != null) curr = curr.next;
+            curr.next = new Node(data);
+        }
+        ++length;
+    }
+
+
+
+//    INSERTION==================================================================================================================
+
+    public void insert(int idx, int data){
+        if(idx <= 0 || idx > ( this.getLength() + 1) ) {
+            System.out.println("Enter a Valid Index!!");
+            return;
+        }
+
+        Node newNode = new Node(data);
+
+        if(idx == 1){
+            newNode.next = head;
+            head = newNode;
+        }
+        else {
+
+            Node curr = head;
+            for(int i=1; i<=idx-2 && curr != null; ++i){
+                curr = curr.next;
+            }
+           if(curr != null){
+               newNode.next = curr.next;
+               curr.next = newNode;
+           }
+        }
+        ++length;
+    }
+
+
+
+
+
+//    Deletion==================================================================================================================
+
+
+    public void delete(int idx){
+        if(idx <= 0 || idx > this.getLength()){
+            System.out.println("Enter a Valid Index!!");
+        }
+        if(head == null) return;
+
+        if(idx == 1){
+            head = head.next;
+        }
+        else {
+            Node curr = head;
+            for(int i=1; i<=idx-2 && curr!=null; ++i){
+                curr = curr.next;
+            }
+            if(curr != null && curr.next != null){
+                curr.next = curr.next.next;
+            }
+        }
+        --length;
+    }
+
+
+
+
+
+//    Deletion==================================================================================================================
+
+
+    public int search(Integer target){
+        int i=1;
+        Node curr = head;
+        while(curr != null){
+            if( target.equals( curr.data ) ) return i;
+            curr = curr.next;
+            ++i;
+        }
+        return -1;
+    }
 }
